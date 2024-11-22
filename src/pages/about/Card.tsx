527de7f120d500db04f2cd59e340062b9f30886f
pegifenu <110061199+pegifenu@users.noreply.github.com>
2024-11-21 19:02:55 -0800

    changed cards

--- src/pages/about/Card.tsx
+++ src/pages/about/Card.tsx
@@ -1,7 +1,14 @@
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
 function Card(props) {
-    return(
+    let pfp_url = "https://placehold.co/150x150"
+    if (props.pfp != "") {
+        pfp_url = '/img/people/' + props.pfp;
+    }
+    
+    return (
         <div className="card">
-            <img className="card-image" src="https://placehold.co/150x150" alt="profile picture"></img>
+            <img className="card-image" src={useBaseUrl(pfp_url)} alt="profile picture"></img>
             <h2 className="card-title">{props.username}</h2>
             <p className="card-text">Placeholder test.</p>
         </div>
--- src/pages/about/Meet-the-team.tsx
+++ src/pages/about/Meet-the-team.tsx
@@ -1,13 +1,13 @@
 import Card from './Card.tsx'
 
 const members = ["Abhiram", "Aidan", "Daniel", "Daphne", "Enrique", "Howard", "Lily", "Matthew"]
-const headshots = []
+const headshots = ["Abhiram.JPG", "", "Daniel.png", "Daphne.JPG", "Enrique.png", "Howard.PNG", "Lily.jpg", "Matthew.jpg"]
 
 export default function Meet(): JSX.Element {
     return (
         <div className="pt-6">
-            {members.map((member) => {
-                return <Card username={member}/>
+            {members.map((member, index) => {
+                return <Card username={member} pfp={headshots[index]}/>
             })}
             
         </div>
