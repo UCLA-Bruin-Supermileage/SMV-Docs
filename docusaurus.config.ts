8f3039d83a041d79492aba75a69bee3b33aa6988
Matthew Tsai <62818122+matthewt-123@users.noreply.github.com>
2025-05-26 11:20:59 -0700

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   url: 'https://smv.seas.ucla.edu',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/info/',
+  baseUrl: '/',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
